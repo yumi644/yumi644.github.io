31429bdc2e48b4a4d6f45d8c5bd40e715dcb3092
yumi <sym740606@gmail.com>
2025-02-02 12:17:01 +0800

    Site updated: 2025-02-02 12:17:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/26/Git学习笔记/","2025/01/26/hello-world/","2025/01/26/前端知识vue3/","2025/01/26/前端知识django/","2025/01/26/前端知识/"];function toRandomPost(){
+var posts=["2025/01/26/Git学习笔记/","2025/01/26/hello-world/","2025/01/26/前端知识vue3/","2025/01/26/前端知识django/","2025/01/26/前端知识/","2025/02/02/思维导图/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
