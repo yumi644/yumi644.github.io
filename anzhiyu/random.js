1d19b35e056f53c1ec9d178cfc9e5a0a1b6f12ea
yumi <sym740606@gmail.com>
2025-02-03 14:53:08 +0800

    Site updated: 2025-02-03 14:53:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html"];function toRandomPost(){
+var posts=["posts/undefined.html","posts/91ce7628.html","posts/7c43b8a8.html","posts/c74774e6.html","posts/f9940255.html","posts/6c27b784.html","posts/a91eaa72.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
